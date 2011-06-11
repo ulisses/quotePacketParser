96048d255d4021bbe953fb20cf84d286fd81df2f
Ulisses Costa <ulissesmonhecosta@gmail.com>
2011-06-11 03:31:58 +0100

    don't parse the quote packet parts we are not interested in. Result of parsing being a Get (MAybe...)

--- QuotePacketParser.hs
+++ QuotePacketParser.hs
@@ -1,166 +1,139 @@
 module QuotePacketParser where
 
-import Text.Parsec
 import Data.Time.LocalTime
-import Text.ParserCombinators.Parsec.Char
 import Data.Binary.Get
+import Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Char8 as BC
+import Data.Char
+import Data.Time.Clock
+import GHC.Int
+import Control.Applicative
 
 data QuotePacket = QuotePacket {
       --Data Type                               2   B6
-    dataType :: Int
       --Information Type                        2   03
-    , infoType :: Int
       --Market Type                             1   4
-    , mrktType :: Int
       --Issue code                             12   ISIN code
-    , issueCode :: Int
+      issueCode :: ByteString
       --Issue seq.-no.                          3
-    , issueSeq :: Int
       --Market Status Type                      2
-    , mrktStatus :: Int
       --Total bid quote volume                  7
-    , totBidVol :: Int
       --Best bid price(1st)                     5
-    , bstBidPrc1 :: Int
+    , bstBidPrc1 :: ByteString
       --Best bid quantity(1st)                  7
-    , bstBidQt1 :: Int
+    , bstBidQt1 :: ByteString
       --Best bid price(2nd)                     5
-    , bstBidPrc2 :: Int
+    , bstBidPrc2 :: ByteString
       --Best bid quantity(2nd)                  7
-    , bstBidQt2 :: Int
+    , bstBidQt2 :: ByteString
       --Best bid price(3rd)                     5
-    , bstBidPrc3 :: Int
+    , bstBidPrc3 :: ByteString
       --Best bid quantity(3rd)                  7
-    , bstBidQt3 :: Int
+    , bstBidQt3 :: ByteString
       --Best bid price(4th)                     5
-    , bstBidPrc4 :: Int
+    , bstBidPrc4 :: ByteString
       --Best bid quantity(4th)                  7
-    , bstBidQt4 :: Int
+    , bstBidQt4 :: ByteString
       --Best bid price(5th)                     5
-    , bstBidPrc5 :: Int
+    , bstBidPrc5 :: ByteString
       --Best bid quantity(5th)                  7
-    , bstBidQt5 :: Int
+    , bstBidQt5 :: ByteString
       --Total ask quote volume                  7
-    , totAsk :: Int
       --Best ask price(1st)                     5
-    , bstAskPrc1 :: Int
+    , bstAskPrc1 :: ByteString
       --Best ask quantity(1st)                  7
-    , bstAskQt1 :: Int
+    , bstAskQt1 :: ByteString
       --Best ask price(2nd)                     5
-    , bstAskPrc2 :: Int
+    , bstAskPrc2 :: ByteString
       --Best ask quantity(2nd)                  7
-    , bstAskQt2 :: Int
+    , bstAskQt2 :: ByteString
       --Best ask price(3rd)                     5
-    , bstAskPrc3 :: Int
+    , bstAskPrc3 :: ByteString
       --Best ask quantity(3rd)                  7
-    , bstAskQt3 :: Int
+    , bstAskQt3 :: ByteString
       --Best ask price(4th)                     5
-    , bstAskPrc4 :: Int
+    , bstAskPrc4 :: ByteString
       --Best ask quantity(4th)                  7
-    , bstAskQt4 :: Int
+    , bstAskQt4 :: ByteString
       --Best ask price(5th)                     5
-    , bstAskPrc5 :: Int
+    , bstAskPrc5 :: ByteString
       --Best ask quantity(5th)                  7
-    , bstAskQt5 :: Int
+    , bstAskQt5 :: ByteString
       --No. of best bid valid quote(total)      5
-    , nrBstBid :: Int
       --No. of best bid quote(1st)              4
-    , nrBstBid1 :: Int
       --No. of best bid quote(2nd)              4
-    , nrBstBid2 :: Int
       --No. of best bid quote(3rd)              4
-    , nrBstBid3 :: Int
       --No. of best bid quote(4th)              4
-    , nrBstBid4 :: Int
       --No. of best bid quote(5th)              4
-    , nrBstBid5 :: Int
       --No. of best ask valid quote(total)      5
-    , nrBstAsk :: Int
       --No. of best ask quote(1st)              4
-    , nrBstQuote1 :: Int
       --No. of best ask quote(2nd)              4
-    , nrBstQuote2 :: Int
       --No. of best ask quote(3rd)              4
-    , nrBstQuote3 :: Int
       --No. of best ask quote(4th)              4
-    , nrBstQuote4 :: Int
       --No. of best ask quote(5th)              4
-    , nrBstQuote5 :: Int
-      -- *Quote accept time*                     8  HHMMSSuu
-    , qat :: TimeZone
+      -- *Quote accept time*                    8  HHMMSSuu
+    , qat :: ByteString
       --End of Message                          1  0xff
-    } deriving (Eq)
+    } deriving Eq
 
+instance Show QuotePacket where
+    show qp = (BC.unpack (qat qp)) ++ " " ++ BC.unpack (issueCode qp) ++ " " 
+           
+           ++ delZeros (bstBidQt5 qp) ++ "@" ++ delZeros (bstBidPrc5 qp) ++ " "
+           ++ delZeros (bstBidQt4 qp) ++ "@" ++ delZeros (bstBidPrc4 qp) ++ " "
+           ++ delZeros (bstBidQt3 qp) ++ "@" ++ delZeros (bstBidPrc3 qp) ++ " "
+           ++ delZeros (bstBidQt2 qp) ++ "@" ++ delZeros (bstBidPrc2 qp) ++ " "
+           ++ delZeros (bstBidQt1 qp) ++ "@" ++ delZeros (bstBidPrc1 qp) ++ " "
 
-example :: String
-example = "B6034KR4201F4292806211000199600023000002300022000000000021000010000020000000000019000000000008930003100000040003200000000003300000000003400000000003500000000001100020000000100000000001090001000000000000000009000000\255"
+           ++ delZeros (bstAskQt1 qp) ++ "@" ++ delZeros (bstAskPrc1 qp) ++ " "
+           ++ delZeros (bstAskQt2 qp) ++ "@" ++ delZeros (bstAskPrc2 qp) ++ " "
+           ++ delZeros (bstAskQt3 qp) ++ "@" ++ delZeros (bstAskPrc3 qp) ++ " "
+           ++ delZeros (bstAskQt4 qp) ++ "@" ++ delZeros (bstAskPrc4 qp) ++ " "
+           ++ delZeros (bstAskQt5 qp) ++ "@" ++ delZeros (bstAskPrc5 qp)
+        where delZeros = delZeros' . Prelude.dropWhile (=='0') . BC.unpack
+              delZeros' s | Prelude.null s = "0"
+                          | otherwise =  s
 
-parseQP :: Get QuotePacket
+humanizeHour = humanizeHour' . Prelude.concatMap appendZero
+humanizeHour' [t1,t2] = [t1,t2]
+humanizeHour' (t1:t2:xs) = [t1 , t2 , ':' ] ++ humanizeHour' xs
+appendZero n = (if n<10 then "0" else "") ++ show n
+
+--parseQP :: Get (Maybe QuotePacket)
 parseQP = do
-    dataType <- getByteString 2
-    infoType <- getByteString 2
-    mrktType <- getByteString 1
-    issueCode <- getByteString 12
-    issueSeq  <- getByteString 3
-    mrktStatus <- getByteString 2
-    totBidVol <- getByteString 7
-    bstBidPrc1 <- getByteString 5
-    bstBidQt1 <- getByteString 7
-    bstBidPrc2 <- getByteString 5
-    bstBidQt2 <- getByteString 7
-    bstBidPrc3 <- getByteString 5
-    bstBidQt3 <- getByteString 7
-    bstBidPrc4 <- getByteString 5
-    bstBidQt4 <- getByteString 7
-    bstBidPrc5 <- getByteString 5
-    bstBidQt5 <- getByteString 7
-    totAsk <- getByteString 7
-    bstAskPrc1 <- getByteString 5
-    bstAskQt1 <- getByteString 7
-    bstAskPrc2 <- getByteString 5
-    bstAskQt2 <- getByteString 7
-    bstAskPrc3 <- getByteString 5
-    bstAskQt3 <- getByteString 7
-    bstAskPrc4 <- getByteString 5
-    bstAskQt4 <- getByteString 7
-    bstAskPrc5 <- getByteString 5
-    bstAskQt5 <- getByteString 7
-    nrBstBid <- getByteString 5
-    nrBstBid1 <- getByteString 4
-    nrBstBid2 <- getByteString 4
-    nrBstBid3 <- getByteString 4
-    nrBstBid4 <- getByteString 4
-    nrBstBid5 <- getByteString 4
-    nrBstAsk <- getByteString 5
-    nrBstQuote1 <- getByteString 4
-    nrBstQuote2 <- getByteString 4
-    nrBstQuote3 <- getByteString 4
-    nrBstQuote4 <- getByteString 4
-    nrBstQuote5 <- getByteString 4
-    qat <- getByteString 8
-    _ <- getByteString 1
-    return $ QuotePacket dataType
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    issueCode <- getBytes 12
+    skip 12
+    bstBidPrc1 <- getBytes 5
+    bstBidQt1 <- getBytes 7
+    bstBidPrc2 <- getBytes 5
+    bstBidQt2 <- getBytes 7
+    bstBidPrc3 <- getBytes 5
+    bstBidQt3 <- getBytes 7
+    bstBidPrc4 <- getBytes 5
+    bstBidQt4 <- getBytes 7
+    bstBidPrc5 <- getBytes 5
+    bstBidQt5 <- getBytes 7
+    skip 7
+    bstAskPrc1 <- getBytes 5
+    bstAskQt1 <- getBytes 7
+    bstAskPrc2 <- getBytes 5
+    bstAskQt2 <- getBytes 7
+    bstAskPrc3 <- getBytes 5
+    bstAskQt3 <- getBytes 7
+    bstAskPrc4 <- getBytes 5
+    bstAskQt4 <- getBytes 7
+    bstAskPrc5 <- getBytes 5
+    bstAskQt5 <- getBytes 7
+    skip 50
+    qat <- getBytes 8
+    eop <- getWord8
+    if eop /= 0xff
+      then do
+        liftA2 (,) (return Nothing) getRemainingLazyByteString
+      else do
+        bs <- getRemainingLazyByteString
+        return $ (Just $ QuotePacket issueCode bstBidPrc1 bstBidQt1
+                         bstBidPrc2 bstBidQt2 bstBidPrc3 bstBidQt3 bstBidPrc4 bstBidQt4 bstBidPrc5 bstBidQt5
+                         bstAskPrc1 bstAskQt1 bstAskPrc2 bstAskQt2 bstAskPrc3 bstAskQt3 bstAskPrc4 bstAskQt4
+                         bstAskPrc5 bstAskQt5 qat,bs)
